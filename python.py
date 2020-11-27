783e5687b73afe37e4133d473dab2812a192bdb9
sakshamgurbhele <64558515+sakshamgurbhele@users.noreply.github.com>
2020-11-27 13:57:12 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+print("lund lelay mera")
+print("chal ht behan kii lodi")
+print("sub chutiye hain")
+##ye uper wali bhasad pratham ki h##
+
+
+Text = ["Saksham", "Pratham", "Kartik", "puneet", "Chaaro Among us Khelete the!"]
+for x in Text:
+  print(x) 
+
